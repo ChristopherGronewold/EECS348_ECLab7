ff838cdf6f91704f839b442f9975c17bc2e5b6c8
ChristopherGronewold <103229059+ChristopherGronewold@users.noreply.github.com>
2023-04-07 14:07:41 -0500

    Add files via upload

--- practice4.php
+++ practice4.php
@@ -1,44 +1,27 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Multiplication Table</title>
-    <style>
-        table, th, td {
-            border: 1px solid black;
-            border-collapse: collapse;
-            padding: 5px;
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
-    <h1>Multiplication Table</h1>
-    <form action="" method="post">
-        Size of the multiplication table: <input type="text" name="size"><br>
-        <input type="submit" name="submit" value="Generate">
-    </form>
-    <?php
-    if (isset($_POST['submit'])) {
-        $size = intval($_POST['size']);
-        if ($size > 0) {
-            echo "<table>";
-            echo "<tr><th></th>";
-            for ($i = 1; $i <= $size; $i++) {
-                echo "<th>$i</th>";
-            }
-            echo "</tr>";
-            for ($i = 1; $i <= $size; $i++) {
-                echo "<tr><th>$i</th>";
-                for ($j = 1; $j <= $size; $j++) {
-                    echo "<td>" . $i * $j . "</td>";
-                }
-                echo "</tr>";
-            }
-            echo "</table>";
-        } else {
-            echo "<p>Please enter a positive integer.</p>";
-        }
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $size = intval($_POST["size"]);
+
+    echo "<table border='1'>";
+
+    // Print the header row
+    echo "<tr>";
+    echo "<th></th>";
+    for ($i = 1; $i <= $size; $i++) {
+        echo "<th>$i</th>";
     }
-    ?>
-</body>
-</html>
+    echo "</tr>";
+
+    // Print the multiplication table
+    for ($i = 1; $i <= $size; $i++) {
+        echo "<tr>";
+        echo "<th>$i</th>";
+        for ($j = 1; $j <= $size; $j++) {
+            echo "<td>" . ($i * $j) . "</td>";
+        }
+        echo "</tr>";
+    }
+
+    echo "</table>";
+}
+?>
